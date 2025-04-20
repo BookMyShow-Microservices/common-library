b357be27c98d1a984911d2d193c207e28b4c09f7
Venu Gopal Rao Dummu <venugopal.dummu@gmail.com>
2025-04-19 19:53:30 -0500

    Add SearchServiceProxy with retry and fallback mechanism
    Introduced `SearchServiceProxy` using OpenFeign with Resilience4j retry and fallback support for fetching show details. Updated `pom.xml` to include dependencies for OpenFeign, validation, and Resilience4j circuit breaker.

---
+++ src/main/java/com/bookmyshow/common/library/proxy/SearchServiceProxy.java
@@ -0,0 +1,27 @@
+package com.bookmyshow.common.library.proxy;
+
+import com.bookmyshow.common.library.model.ShowDetails;
+import io.github.resilience4j.retry.annotation.Retry;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "search-service")
+public interface SearchServiceProxy {
+	
+	@GetMapping("/api/showdetails")
+	//@CircuitBreaker(name = "fetchShowDetailsCircuitBreaker", fallbackMethod = "fetchShowDetailsFallback")
+	@Retry(name = "fetchShowDetailsRetries", fallbackMethod = "fetchShowDetailsFallback")
+	public ResponseEntity<ShowDetails> searchShowDetails(@RequestParam @NotNull Integer showId);
+	
+	default ResponseEntity<ShowDetails> fetchShowDetailsFallback(@RequestParam @NotNull Integer showId, Throwable e) {
+		System.out.println("Retry is being called and fallback method got triggered: " + e.getMessage());
+		//System.out.println("Circuit breaker fallback method triggered: " + e.getMessage());
+		ShowDetails showDetails = new ShowDetails();
+		showDetails.setShowId(showId);
+	    return ResponseEntity.ok(showDetails);
+	}
+
+}
